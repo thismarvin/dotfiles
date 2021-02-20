5e1a372d1bb43e403f677f78d78368f0428cd93f
marvin <marvin@thismarvin.com>
2021-02-20 16:54:33 -0600

    added a utils.lua for common functions

---
+++ .config/nvim/lua/utils.lua
@@ -0,0 +1,19 @@
+local Utils = {}
+
+local command = vim.api.nvim_command
+
+Utils.map = vim.api.nvim_set_keymap
+
+function Utils.execute(expression)
+    vim.api.nvim_exec(expression, true)
+end
+
+function Utils.has(key)
+    return vim.fn.has(key) == 1
+end
+
+function Utils.set(expression)
+    command(string.format("set " .. expression))
+end
+
+return Utils
