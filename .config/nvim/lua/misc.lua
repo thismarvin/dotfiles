9162b63e30f5abf4523a16ea8522682d918adf19
marvin <marvin@thismarvin.com>
2021-02-20 18:03:59 -0600

    made a file for misc options

---
+++ .config/nvim/lua/misc.lua
@@ -0,0 +1,14 @@
+local map = require("utils").map
+local execute = require("utils").execute
+
+-- Allows vim-rooter and startify to coexist.
+execute([[
+augroup startify_test
+  autocmd!
+  autocmd User StartifyBufferOpened :Rooter
+augroup end
+]])
+
+-- Mappings for my jank plugin!
+map("n", "<C-t>", [[:lua require("nvim-term").toggle()<CR>]], { noremap=true, silent=true })
+map("t", "<C-t>", [[<C-\><C-n>:lua require("nvim-term").toggle()<CR>]], { noremap=true, silent=true })
