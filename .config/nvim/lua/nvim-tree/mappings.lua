4e2ceb778ccc676ac8769aeac87291fd29965890
marvin <marvin@thismarvin.com>
2021-02-24 23:51:54 -0600

    tweaked nvim tree settings

--- .config/nvim/lua/nvim-tree/mappings.lua
+++ .config/nvim/lua/nvim-tree/mappings.lua
@@ -1,3 +1,4 @@
 local map = require("utils").map
 
 map("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+map("n", "<leader>E", ":NvimTreeRefresh<CR>", { noremap = true, silent = true })
--- .config/nvim/lua/nvim-tree/settings.lua
+++ .config/nvim/lua/nvim-tree/settings.lua
@@ -1,3 +1,5 @@
 vim.g.nvim_tree_width = 25
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_disable_netrw = 0
+vim.g.nvim_tree_hijack_netrw = 0
