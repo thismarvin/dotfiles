7238828fa4edd7820061218462913d9dff7e0d95
marvin <marvin@thismarvin.com>
2021-02-24 23:53:34 -0600

    Ctrl-Space to toggle completion!

--- .config/nvim/lua/nvim-compe/mappings.lua
+++ .config/nvim/lua/nvim-compe/mappings.lua
@@ -1,4 +1,4 @@
 local map = require("utils").map
 
 map("i", "<CR>", [[compe#confirm("<CR>")]], { noremap = true, silent = true, expr = true })
-map("i", "<C-c>", "compe#complete()", { noremap = true, silent = true, expr = true })
+map("i", "<C-Space>", "compe#complete()", { noremap = true, silent = true, expr = true })
