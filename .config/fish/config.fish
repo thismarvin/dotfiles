a4d5e7eb151ec6d60beb63f0366972324d31178a
marvin <marvin@thismarvin.com>
2021-03-12 22:46:44 -0600

    reworked how I set fish colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,46 @@ set fish_add_path $HOME/.local/bin
 set fish_add_path $HOME/.cargo/bin
 
 set -U fish_greeting ""
-set -U fish_color_command 7daea3 #7daea3
-set -U fish_color_error ea6962 #ea6962
-set -U fish_color_quote e78a4e #e78a4e
-set -U fish_color_comment e78a4e #e78a4e
-set -U fish_color_autosuggestion 928374 #928374
+
+# Set all fish colors to white.
+set -U fish_color_normal white
+set -U fish_color_status white
+set -U fish_color_command white
+set -U fish_color_quote white
+set -U fish_color_redirection white
+set -U fish_color_end white
+set -U fish_color_error white
+set -U fish_color_param white
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match white --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator white
+set -U fish_color_escape white
+set -U fish_color_cwd white
+set -U fish_color_cwd_root white
+set -U fish_color_valid_path white --underline
+set -U fish_color_autosuggestion white brblack
+set -U fish_color_user white
+set -U fish_color_host white
+set -U fish_color_host_remote white
+set -U fish_color_cancel -r
+set -U fish_pager_color_completion white
+set -U fish_pager_color_description white yellow
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress white --background=cyan
+set -U fish_color_match --background=brblue
+set -u fish_color_comment white
+
+# Now just apply the colors I like.
+set -U fish_color_command blue
+set -U fish_color_error red
+set -U fish_color_quote orange
+set -U fish_color_comment orange
+set -U fish_color_autosuggestion brblack
+
+function fish_user_key_bindings
+  fish_vi_key_bindings
+end
 
 set -gx EDITOR nvim
 set -gx FZF_DEFAULT_COMMAND "fd --type f"
