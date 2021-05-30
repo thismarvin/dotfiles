24a0178e97154a5102822858ee4a62061a266aa1
marvin <marvin@thismarvin.com>
2021-05-29 21:22:15 -0500

    better hop search

--- .config/nvim/lua/plugins/hop/mappings.lua
+++ .config/nvim/lua/plugins/hop/mappings.lua
@@ -2,6 +2,6 @@ local map = require("utils").map
 
 map("n", "<leader><Up>", ":HopLine<CR>", { noremap = true, silent = true })
 map("n", "<leader><Down>", ":HopLine<CR>", { noremap = true, silent = true })
-map("n", "<leader>s", ":HopChar1<CR>", { noremap = true, silent = true })
+map("n", "<leader>/", ":HopPattern<CR>", { noremap = true, silent = true })
 map("v", "<leader><Up>", "<cmd>HopLine<CR>", { noremap = true, silent = true })
 map("v", "<leader><Down>", "<cmd>HopLine<CR>", { noremap = true, silent = true })
