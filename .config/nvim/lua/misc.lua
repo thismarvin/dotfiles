bb1687e2e163fa92de670d5e60a72d8a574149d4
marvin <marvin@thismarvin.com>
2021-05-29 21:21:44 -0500

    added misc scratchpad mapping

--- .config/nvim/lua/misc.lua
+++ .config/nvim/lua/misc.lua
@@ -4,3 +4,5 @@ require "plugins.thismarvin.surround"
 
 map("n", "gss", "<Cmd>lua require('plugins.thismarvin.surround').surround_line()<CR>", { noremap = true, silent = true })
 map("n", "gs", "<Cmd>set operatorfunc=SurroundOperatorCustom<CR>g@", { noremap = true, silent = true })
+
+map("n", "<leader>md", "<Cmd>sp | e ~/sratchpad.md | setlocal spell | setlocal wrap<CR>", { noremap = true, silent = true })
