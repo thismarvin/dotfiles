07752b08de7f08242d9663513480faea9396e398
marvin <marvin@thismarvin.com>
2021-06-10 11:23:54 -0500

    learned how to spell scratch

--- .config/nvim/lua/misc.lua
+++ .config/nvim/lua/misc.lua
@@ -5,4 +5,4 @@ require "plugins.thismarvin.surround"
 map("n", "gss", "<Cmd>lua require('plugins.thismarvin.surround').surround_line()<CR>", { noremap = true, silent = true })
 map("n", "gs", "<Cmd>set operatorfunc=SurroundOperatorCustom<CR>g@", { noremap = true, silent = true })
 
-map("n", "<leader>md", "<Cmd>sp | e ~/sratchpad.md | setlocal spell | setlocal wrap<CR>", { noremap = true, silent = true })
+map("n", "<leader>md", "<Cmd>sp | e ~/scratchpad.md | setlocal spell | setlocal wrap<CR>", { noremap = true, silent = true })
