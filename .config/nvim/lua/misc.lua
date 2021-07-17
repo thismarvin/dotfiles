6aad6e819c17d8ed1fc2e98f0a40e0d8a84fb524
marvin <marvin@thismarvin.com>
2021-07-16 22:18:17 -0500

    added autocmd for gitcommit file types

--- .config/nvim/lua/misc.lua
+++ .config/nvim/lua/misc.lua
@@ -1,3 +1,4 @@
+local execute = require("utils").execute
 local map = require("utils").map
 
 require "plugins.thismarvin.surround"
@@ -6,3 +7,10 @@ map("n", "gss", "<Cmd>lua require('plugins.thismarvin.surround').surround_line()
 map("n", "gs", "<Cmd>set operatorfunc=SurroundOperatorCustom<CR>g@", { noremap = true, silent = true })
 
 map("n", "<leader>md", "<Cmd>sp | e ~/scratchpad.md | setlocal spell | setlocal wrap<CR>", { noremap = true, silent = true })
+
+execute([[
+augroup misc
+  autocmd!
+	autocmd FileType gitcommit setlocal spell | set cc=74
+augroup end
+]])
