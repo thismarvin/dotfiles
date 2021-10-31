d357c88e2a19a065370aa3a32f3ed7745514625e
marvin <marvin@thismarvin.com>
2021-10-31 14:52:07 -0500

    removed custom gruvbox material function

--- nvim/.config/nvim/lua/styling/theme.lua
+++ nvim/.config/nvim/lua/styling/theme.lua
@@ -15,22 +15,4 @@ function! TransparentMode()
   hi NonText guibg=NONE guifg=NONE ctermbg=NONE ctermfg=NONE
   hi EndOfBuffer guibg=NONE ctermbg=NONE
 endfunction
-
-function! ThemeGruvboxMaterial()
-  let g:gruvbox_material_background = "hard"
-
-  colorscheme gruvbox-material
-
-  hi LspDiagnosticsVirtualTextError guifg=#ea6962
-  hi LspDiagnosticsVirtualTextWarning guifg=#d8a657
-  hi LspDiagnosticsVirtualTextInformation guifg=#89b482
-  hi LspDiagnosticsVirtualTextHint guifg=#89b482
-
-  hi HopNextKey guifg=#ea6962 gui=NONE
-  hi HopNextKey1 guifg=#7daea3 gui=NONE
-  hi HopNextKey2 guifg=#e78a4e gui=NONE
-  hi HopUnmatched guifg=#928374 gui=NONE
-	
-	hi IndentBlanklineContextChar guifg=#928374
-endfunction
 ]])
