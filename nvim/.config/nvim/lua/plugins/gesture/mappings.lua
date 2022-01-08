258ba95a80166faa35712730733eede61eec4b59
marvin <marvin@thismarvin.com>
2022-01-08 13:33:57 -0600

    removed unused plugins

--- nvim/.config/nvim/lua/plugins/gesture/mappings.lua
+++
@@ -1,5 +0,0 @@
-local map = require("utils").map
-
-map("n", "<RightMouse>", "<Nop>", { noremap = true, silent = true })
-map("n", "<RightDrag>", "<Cmd>lua require('gesture').draw()<CR>", { noremap = true, silent = true })
-map("n", "<RightRelease>", "<Cmd>lua require('gesture').finish()<CR>", { noremap = true, silent = true })
