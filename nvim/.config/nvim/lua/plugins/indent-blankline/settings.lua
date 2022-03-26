1855fbd481a4f613161bdd2fbf4ec43461e69053
marvin <me@thismarvin.com>
2022-03-26 13:44:28 -0500

    added man to exclude list

--- nvim/.config/nvim/lua/plugins/indent-blankline/settings.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline/settings.lua
@@ -2,5 +2,5 @@ require("indent_blankline").setup({
 	char = "┊", -- "│"
 	show_trailing_blankline_indent = false,
 	buftype_exclude = { "nofile", "terminal" },
-	filetype_exclude = { "gesture", "help", "packer" },
+	filetype_exclude = { "help", "packer", "man" },
 })
