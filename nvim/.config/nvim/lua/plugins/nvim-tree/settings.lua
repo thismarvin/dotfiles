bb075252f8d8614ba54391c267d53eba53455f7f
marvin <me@thismarvin.com>
2022-04-11 12:51:46 -0500

    enable indent markers via lua

--- nvim/.config/nvim/lua/plugins/nvim-tree/settings.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree/settings.lua
@@ -1,6 +1,10 @@
-vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_git_hl = 1
 
 require("nvim-tree").setup({
 	hijack_netrw = false,
+	renderer = {
+		indent_markers = {
+			enable = true,
+		},
+	},
 })
