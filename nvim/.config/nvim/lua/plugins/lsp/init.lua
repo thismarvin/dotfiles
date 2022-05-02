418d3b881e9730515da0e6fa842e08c0d486ef17
marvin <me@thismarvin.com>
2022-05-02 18:19:42 -0500

    clean up cmp settings

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,35 +1,21 @@
 local M = {}
 
 function M.on_attach(_, bufnr)
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	local options = { buffer = bufnr, silent = true }
 
-	vim.cmd([[ command! Format execute "lua vim.lsp.buf.formatting()" ]])
-
-	local map = vim.api.nvim_buf_set_keymap
-	local options = { noremap = true, silent = true }
-
-	map(bufnr, "n", "]d", "<Cmd>lua vim.diagnostic.goto_next({ float = { border = 'rounded' }})<CR>zz", options)
-	map(bufnr, "n", "[d", "<Cmd>lua vim.diagnostic.goto_prev({ float = { border = 'rounded' }})<CR>zz", options)
-	map(bufnr, "n", "<Leader>d", "<Cmd>lua vim.diagnostic.open_float(nil, { border = 'rounded' })<CR>", options)
-	map(bufnr, "n", "<Leader>k", "<Cmd>lua vim.lsp.buf.hover()<CR>", options)
-	map(bufnr, "n", "<Leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", options)
-	map(bufnr, "n", "<Leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", options)
-	map(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>zz", options)
-	map(bufnr, "n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>zz", options)
-	map(bufnr, "n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>zz", options)
+	vim.keymap.set("n", "]d", "<Cmd>lua vim.diagnostic.goto_next({ float = { border = 'rounded' }})<CR>zz", options)
+	vim.keymap.set("n", "[d", "<Cmd>lua vim.diagnostic.goto_prev({ float = { border = 'rounded' }})<CR>zz", options)
+	vim.keymap.set("n", "<Leader>d", "<Cmd>lua vim.diagnostic.open_float(nil, { border = 'rounded' })<CR>", options)
+	vim.keymap.set("n", "<Leader>k", vim.lsp.buf.hover, options)
+	vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, options)
+	vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, options)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, options)
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, options)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, options)
 end
 
-M.capabilities = vim.lsp.protocol.make_client_capabilities()
-M.capabilities.textDocument.completion.completionItem.snippetSupport = true
-M.capabilities.textDocument.completion.completionItem.resolveSupport = {
-	properties = {
-		"documentation",
-		"detail",
-		"additionalTextEdits",
-	},
-}
-
-M.capabilities = require("cmp_nvim_lsp").update_capabilities(M.capabilities)
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+M.capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
 M.handlers = {
 	["textDocument/publishDiagnostics"] = vim.lsp.with(
--- nvim/.config/nvim/lua/plugins/luasnip/mappings.lua
+++ nvim/.config/nvim/lua/plugins/luasnip/mappings.lua
@@ -1,37 +1,20 @@
 local luasnip = require("luasnip")
 
-local t = function(str)
-	return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-	local col = vim.fn.col(".") - 1
-	if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-		return true
-	else
-		return false
-	end
-end
-
-_G.forward = function()
+local function forward()
 	if luasnip and luasnip.expand_or_jumpable() then
-		return t("<Plug>luasnip-expand-or-jump")
-	elseif check_back_space() then
-		return t("<C-e>")
+		return "<Plug>luasnip-expand-or-jump"
 	end
+
 	return ""
 end
 
-_G.backward = function()
+local function backward()
 	if luasnip and luasnip.jumpable(-1) then
-		return t("<Plug>luasnip-jump-prev")
-	else
-		return t("<C-n>")
+		return "<Plug>luasnip-jump-prev"
 	end
+
 	return ""
 end
 
-vim.api.nvim_set_keymap("i", "<C-e>", "v:lua.forward()", { expr = true })
-vim.api.nvim_set_keymap("s", "<C-e>", "v:lua.forward()", { expr = true })
-vim.api.nvim_set_keymap("i", "<C-n>", "v:lua.backward()", { expr = true })
-vim.api.nvim_set_keymap("s", "<C-n>", "v:lua.backward()", { expr = true })
+vim.keymap.set({ "i", "s" }, "<C-n>", forward, { expr = true })
+vim.keymap.set({ "i", "s" }, "<C-p>", backward, { expr = true })
