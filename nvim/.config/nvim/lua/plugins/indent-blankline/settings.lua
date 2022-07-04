37e192433c6d73c3e8e175ba5f30e2ae39306b9c
marvin <me@thismarvin.com>
2022-07-03 22:25:59 -0500

    add `diff` to exclude list

--- nvim/.config/nvim/lua/plugins/indent-blankline/settings.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline/settings.lua
@@ -3,4 +3,13 @@ require("indent_blankline").setup({
 	-- ┃ ╏ ┇ ┋
 	char_list = { "│", "╎" },
 	show_trailing_blankline_indent = false,
+	filetype_exclude = {
+		"",
+		"checkhealth",
+		"diff",
+		"help",
+		"lspinfo",
+		"man",
+		"packer",
+	},
 })
