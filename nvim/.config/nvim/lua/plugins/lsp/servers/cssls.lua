2991d87f41376f19f7a7990aa956d3e4f95af9ee
Marvin Valenzuela <me@thismarvin.com>
2022-07-24 11:26:50 -0500

    change `cmd` to work with nix

--- nvim/.config/nvim/lua/plugins/lsp/servers/cssls.lua
+++ nvim/.config/nvim/lua/plugins/lsp/servers/cssls.lua
@@ -2,4 +2,5 @@ require("lspconfig").cssls.setup({
 	on_attach = require("plugins.lsp").on_attach,
 	capabilities = require("plugins.lsp").capabilities,
 	handlers = require("plugins.lsp").handlers,
+	cmd = { "css-languageserver", "--stdio" },
 })
--- nvim/.config/nvim/lua/plugins/lsp/servers/html.lua
+++ nvim/.config/nvim/lua/plugins/lsp/servers/html.lua
@@ -2,4 +2,5 @@ require("lspconfig").html.setup({
 	on_attach = require("plugins.lsp").on_attach,
 	capabilities = require("plugins.lsp").capabilities,
 	handlers = require("plugins.lsp").handlers,
+	cmd = { "html-languageserver", "--stdio" },
 })
